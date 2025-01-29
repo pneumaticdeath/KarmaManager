b7afdeeddc53377ad6b12c18fd2e7cd4dc56b931
Mitch Patenaude <mitch@mitchpatenaude.net>
2025-01-29 11:11:32 -0800

    New offensive words, searching, build numbers

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -11,6 +12,8 @@ import (
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
+
+const searchLimit = 100000
 
 func main() {
 	App := app.NewWithID("io.patenaude.karmamanager")
@@ -61,7 +64,7 @@ func main() {
 
 	controlBar := container.New(layout.NewAdaptiveGridLayout(3), inputEntry, mainSelect, addedDictsContainer)
 
-	mainDisplay := widget.NewList(func() int { // list length
+	resultsDisplay := widget.NewList(func() int { // list length
 		return resultSet.Count()
 	}, func() fyne.CanvasObject { // Make new entry
 		return widget.NewLabel("Foo")
@@ -75,6 +78,58 @@ func main() {
 		// fmt.Println(index, " ", text)
 		object.Refresh()
 	})
+
+	lastSearchIndex := -1
+	lastSearchText := ""
+	searchError := widget.NewLabel("")
+	searchbox := widget.NewEntry()
+	searchbox.OnSubmitted = func(searchfor string) {
+		searchError.Text = ""
+		searchError.Refresh()
+		if searchfor == "" {
+			lastSearchIndex = -1
+			return
+		}
+		searchstring := strings.ToLower(searchfor)
+		searchError.Text = fmt.Sprintf("Searching for '%s'", searchstring)
+		searchError.Refresh()
+		i := 0
+		if lastSearchText == searchstring {
+			i = lastSearchIndex + 1
+		} else {
+			lastSearchText = searchstring
+			lastSearchIndex = -1
+		}
+		for i < resultSet.Count() && i < searchLimit {
+			text, _ := resultSet.GetAt(i)
+			if strings.Index(strings.ToLower(text), searchstring) != -1 {
+				resultsDisplay.ScrollTo(i)
+				resultsDisplay.Refresh()
+				searchError.Text = fmt.Sprintf("Found '%s' at %d", searchstring, i+1)
+				searchError.Refresh()
+				lastSearchIndex = i
+				return
+			}
+			i += 1
+		}
+		if resultSet.IsDone() && i >= resultSet.Count() {
+			if lastSearchIndex >= 0 {
+				searchError.Text = fmt.Sprintf("'%s' not found after %d, starting over.",
+					searchstring, lastSearchIndex+1)
+				lastSearchIndex = -1
+			} else {
+				searchError.Text = "Not Found!"
+			}
+		} else if i >= searchLimit {
+			searchError.Text = fmt.Sprintf("Didn't find '%s' in the first %d results!", searchstring, searchLimit)
+			lastSearchIndex = i
+		}
+		searchError.Refresh()
+	}
+
+	advControls := container.New(layout.NewVBoxLayout(), widget.NewLabel("Search for:"), searchbox, searchError)
+
+	mainDisplay := container.New(layout.NewAdaptiveGridLayout(2), resultsDisplay, advControls)
 
 	content := container.NewBorder(controlBar, nil, nil, nil, mainDisplay)
 
