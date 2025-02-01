0e4d55fa7189432fca55924840d2e6210bd97177
Mitch Patenaude <mitch@mitchpatenaude.net>
2025-01-31 22:52:53 -0800

    Latest mobile builds

--- release_mobile.sh
+++ release_mobile.sh
@@ -5,7 +5,7 @@ set -e
 source .env 
 
 rm -f KarmaManager.aab
-fyne release --keyStore "${ANDROIDKEYSTORE}" --keyName "${ANDROIDKEYNAME}" --target android
+fyne release --keyStore ${ANDROIDKEYSTORE} --keyName "${ANDROIDKEYNAME}" --target android
 
 rm -f KarmaManager.ipa
-fyne release --target ios --certificate "${IOSDISTROCERT}" --profile "${IOSDDISTROPROF}"
+fyne release --target ios --certificate "${IOSDISTROCERT}" --profile "${IOSDISTROPROF}"
