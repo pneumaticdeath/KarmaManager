e440438f0ccda4116f90e3eee0c71552e53864c6
Mitch Patenaude <mitch@mitchpatenaude.net>
2025-01-31 21:26:40 -0800

    release and testing scripts with secrets removed

---
+++ release_mobile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+source .env 
+
+rm -f KarmaManager.aab
+fyne release --keyStore "${ANDROIDKEYSTORE}" --keyName "${ANDROIDKEYNAME}" --target android
+
+rm -f KarmaManager.ipa
+fyne release --target ios --certificate "${IOSDISTROCERT}" --profile "${IOSDDISTROPROF}"
