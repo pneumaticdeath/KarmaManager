325fbcdc769a44ed285a864dcdf0fea32957449e
Mitch Patenaude <mitch@mitchpatenaude.net>
2025-02-12 15:53:36 -0800

    Changes to build script, mobile release numbers

--- release_mobile.sh
+++ release_mobile.sh
@@ -5,7 +5,7 @@ set -e
 source .env 
 
 rm -f KarmaManager.aab
-fyne release --keyStore ${ANDROIDKEYSTORE} --keyName "${ANDROIDKEYNAME}" --target android
+fyne release --key-store ${ANDROIDKEYSTORE} --key-name "${ANDROIDKEYNAME}" --key-pass "${ANDROIDKEYPASS}" --target android
 
 rm -f KarmaManager.ipa
 fyne release --target ios --certificate "${IOSDISTROCERT}" --profile "${IOSDISTROPROF}"
