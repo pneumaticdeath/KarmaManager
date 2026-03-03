d13955512381ccf9b36fed65f6ca7ceac9d5b589
Mitch Patenaude <mitch@mitchpatenaude.net>
2026-03-02 22:55:43 -0800

    Mobile build automation

--- release_mobile.sh
+++ release_mobile.sh
@@ -5,7 +5,7 @@ set -e
 source .env 
 
 rm -f KarmaManager.aab
-fyne release --key-store ${ANDROIDKEYSTORE} --key-name "${ANDROIDKEYNAME}" --key-pass "${ANDROIDKEYPASS}" --target android
+fyne release --keystore ${ANDROIDKEYSTORE} --key-name "${ANDROIDKEYNAME}" --key-pass "${ANDROIDKEYPASS}" --target android
 
 rm -f KarmaManager.ipa
 fyne release --target ios --certificate "${IOSDISTROCERT}" --profile "${IOSDISTROPROF}"
